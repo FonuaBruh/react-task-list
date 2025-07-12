f3576734ed3fba535207d9abdf592b77f08bdd8a
FonuaBruh <frizkoksslow@gmail.com>
2025-07-13 00:42:59 +0300

    update UI

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./note.css";
 
 function Note({ text, onDelete }) {
-    return(
-        <div className="note__container">
-            <p className="note__text">{text}</p>
-			<button onClick={onDelete}>×</button>
-        </div>
-    );
+	return (
+		<div className="note">
+			<div className="note__container">
+				<p className="note__text">{text}</p>
+			</div>
+			<button className="delete-button" onClick={onDelete}>X</button>
+		</div>
+	);
 }
 
 export default Note;
