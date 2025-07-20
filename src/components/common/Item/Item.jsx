2f47323b592008c34cd85472d206815dc986e919
FonuaBruh <frizkoksslow@gmail.com>
2025-07-20 18:43:18 +0300

    add useful buttons to the every note

--- src/components/common/Item/Item.jsx
+++ src/components/common/Item/Item.jsx
@@ -25,6 +25,14 @@ const Item = ({
 			<div className="text">
 				{item.title}
 			</div>
+			<div className="item-tools">
+				<p className="done-button">✔</p>
+				<div className="move-note">
+					<p className="move-right-button">←</p>
+					<p className="move-left-button">→</p>
+				</div>
+				<p className="delete-button">×</p>
+			</div>
 		</div>
 	);
 };
